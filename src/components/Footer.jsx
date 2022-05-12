ca7a18991f72c61da155d95d5bc50c817481b342
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-05-12 17:36:49 +0200

    ajout du formulaire de contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import "../styles/Footer.css";
+import facebook from "../assets/picto-fb.png";
+import instagram from "../assets/picto-insta.png";
 
 const Footer = () => {
     return (
         <footer>
         <div>
-            
+        <div className="pictos">
+          <img src={facebook} alt="picto facebook" />
+          <img src={instagram} alt="picto instagram" />
+          </div>
         </div>
         </footer>
     );
