70e33777eb3afd4bca836a73d25103cd3e6ff0b0
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-05-12 14:15:00 +0200

    ajout images + mise en place header/css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
       <div>
-        <img alt="logo" src={'src/assets/Logo-rond.png'} />
         <p>Salut ça farte ?!</p>
       </div>
     </div>
