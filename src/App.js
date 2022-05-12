0510882e96868f5e0f294567b6f6144789f87dce
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-05-12 15:25:24 +0200

    css header + creation variables couleurs + mise en place footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <p>Salut ça farte ?!</p>
       </div>
+      <Footer />
     </div>
   );
 }
