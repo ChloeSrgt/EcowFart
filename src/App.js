87d4037ba95357dcacb82b588e609224ce3bbd4e
Nathan Conty <83788253+Natecty@users.noreply.github.com>
2022-05-12 16:45:18 +0200

    maj BDD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Donuts from './components/DonutsMethane';
 import  {Ble}  from './BDD/DataBle';
+import {Lin} from './BDD/DataLin';
+import { useState } from 'react';
 
 function App() {
+  const [cereales,setCereales]=useState(Lin);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +14,12 @@ function App() {
       <div>
         <img alt="logo" src={'src/assets/Logo-rond.png'} />
         <p>Salut ça farte ?!</p>
+        <button type='button' onClick={() => setCereales(Ble)}> Lin </button>
+        
       </div>
       <div style={{width: 300}}>
-    <Donuts Database={Ble}/>
+    <Donuts Database={cereales}/>
+    <Donuts Database={cereales}/>
       </div>
     </div>
   );
