5a25c2526ecc9388d8e26225d061f9ac78870076
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-05-13 12:07:25 +0200

    kiki

--- src/components/FirstHomeContainer.jsx
+++ src/components/FirstHomeContainer.jsx
@@ -7,8 +7,10 @@ const FirstHomeContainer = () => {
         <div className="home-container">
             <img className="container-img" src={methagif} alt="gif vache qui pète" />
             <div className="text-container">
-            <h3>Abbohgdoirhjdiohj</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa fugiat adipisci, velit nihil repudiandae incidunt iste cupiditate mollitia sapiente corporis voluptatibus consequatur, debitis repellat error eveniet ratione harum provident assumenda.</p>
+            <h3>Que se passe-t-il ?</h3>
+            <h4>Saviez-vous que les vaches françaises émettent autant de gaz en un an que 15 millions de voitures ?</h4>
+            <p>Et oui, l’industrie des bovins de boucherie et des vaches laitières est l’un des principaux contributeurs aux gaz à effet de serre dans le monde. L'alimentation des vaches a un grand effet sur la quantité de méthane qu’elles produisent. En effet, la digestion de certains types d’aliments produit plus de méthane que d'autres.
+            </p>
             </div>
         </div>
     );
